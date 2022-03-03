806d29034706b80d2e86077df72cca4aac42627b
andreasgottardi <andreas.gottardi@gmx.at>
2022-03-03 19:33:41 +0100

    Fixing apt.

--- InstallPreRequisites.sh
+++ InstallPreRequisites.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-apt install -y pandoc mkdocs pdflatex pandoc mkdocs pandoc mkdocs git -y texlive-latex-recommended texlive-latex-extra librsvg2-bin texlive-fonts-extra
+apt install -y pandoc mkdocs pandoc mkdocs pandoc mkdocs texlive-latex-recommended texlive-latex-extra librsvg2-bin texlive-fonts-extra
 
 wget -O /tmp/Eisvogel-2.0.0.tar.gz https://github.com/Wandmalfarbe/pandoc-latex-template/releases/download/v2.0.0/Eisvogel-2.0.0.tar.gz
 mkdir ~/.pandoc/templates -p
